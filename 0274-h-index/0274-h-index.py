a472aeee20fe3beedfa1ad4e8780f9ff1fee7508
윤영진 <141153492+0-Jhin@users.noreply.github.com>
2025-03-31 20:51:45 +0900

    Time: 0 ms (100%), Space: 12.8 MB (34.98%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def hIndex(self, citations):
-        """
-        :type citations: List[int]
-        :rtype: int
-        """
         citations.sort()
         L = len(citations)
         for i in range(L):
