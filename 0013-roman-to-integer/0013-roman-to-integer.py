52b63bf7948ee659dec8967ef093558aafc2bb07
윤영진 <141153492+0-Jhin@users.noreply.github.com>
2025-04-29 11:59:28 +0900

    Time: 4 ms (86.4%), Space: 12.5 MB (54.42%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -2,12 +2,10 @@ class Solution(object):
     def romanToInt(self, s):
         rs = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
         r = 0
-        pre = 0
-        for i in reversed(s):
-            if rs[i] < pre:
-                r -= rs[i]
+        for i in range(len(s)-1):
+            if rs[s[i]] < rs[s[i+1]]:
+                r -= rs[s[i]]
             else:
-                r += rs[i]
-                pre = rs[i]
-            print(r, i, pre)
+                r += rs[s[i]]
+        r += rs[s[-1]]
         return r
